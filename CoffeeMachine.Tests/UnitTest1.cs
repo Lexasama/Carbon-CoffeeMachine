b6295fc568d09e4088178dd05899e6fc95ffe121
axel-sanon <axel.sanon@carbon-it.com>
2022-12-12 11:57:52 +0100

    init project

---
+++ CoffeeMachine.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace CoffeeMachine.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ CoffeeMachine/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace CoffeeMachine;
+
+public class Class1
+{
+}
